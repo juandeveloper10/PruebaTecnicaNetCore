9320b282aa491ce35e462f6cc3f311a47ad5d765
juandeveloper10 <77118167+juandeveloper10@users.noreply.github.com>
2025-06-13 16:45:03 -0600

    Add files via upload

---
+++ Entidades/Escuela.cs
@@ -0,0 +1,9 @@
+﻿namespace EscuelaMusica.Entidades
+{
+    public class Escuela
+    {
+        public int Escuela_Id { get; set; } 
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Entidades/Profesor.cs
@@ -0,0 +1,11 @@
+﻿namespace EscuelaMusica.Entidades
+{
+    public class Profesor
+    {
+        public int Profesor_Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public int Profesor_Escuela { get; set; }
+    }
+}
