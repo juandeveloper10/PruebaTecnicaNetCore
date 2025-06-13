a48e53112b65de81c803892f4985c1f3d4b3d996
juandeveloper10 <77118167+juandeveloper10@users.noreply.github.com>
2025-06-13 16:44:34 -0600

    Add files via upload

---
+++ Controllers/EscuelaController.cs
@@ -0,0 +1,50 @@
+﻿using EscuelaMusica.Entidades;
+using EscuelaMusica.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EscuelaMusica.Controllers
+{
+    public class EscuelaController(IEscuela _repository) : Controller
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Escuela escuela)
+        {
+            await _repository.InsertEscuela(escuela);
+            return CreatedAtAction(nameof(GetById), new { id = escuela.Escuela_Id }, escuela);
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Escuela>> GetAll()
+        {
+            return await _repository.GetEscuela();
+
+        }
+        [HttpGet("{id: int}")]
+        public async Task<Escuela> GetById(int id)
+        {
+            return await _repository.GetEscuelaById(id);
+
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] Escuela escuela)
+        {
+            await _repository.UpdateEscuela(escuela);
+            return NoContent();
+
+        }
+
+        [HttpPut("{id: int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _repository.DeleteEscuela(id);
+            return NoContent();
+
+        }
+
+
+
+    }
+
+
+}
---
+++ Controllers/ProfesorController.cs
@@ -0,0 +1,48 @@
+﻿using EscuelaMusica.Entidades;
+using EscuelaMusica.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EscuelaMusica.Controllers
+{
+    public class ProfesorController(IProfesor _repository) : Controller
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Profesor profesor)
+        {
+            await _repository.InsertProfesor(profesor);
+            return CreatedAtAction(nameof(GetById), new { id = profesor.Profesor_Id }, profesor);
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Profesor>> GetAll()
+        {
+            return (IEnumerable<Profesor>)await _repository.GetProfesor();
+
+        }
+        [HttpGet("{id: int}")]
+        public async Task<Profesor> GetById(int id)
+        {
+            return await _repository.GetProfesorById(id);
+
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] Profesor profesor)
+        {
+            await _repository.UpdateProfesor(profesor);
+            return NoContent();
+
+        }
+
+        [HttpPut("{id: int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _repository.DeleteProfesor(id);
+            return NoContent();
+
+        }
+
+
+
+    }
+}
