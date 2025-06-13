b88b7e1c924299968a4b44620e5e20cfc6b5a189
juandeveloper10 <77118167+juandeveloper10@users.noreply.github.com>
2025-06-13 16:45:19 -0600

    Add files via upload

---
+++ Interfaces/IEscuela.cs
@@ -0,0 +1,14 @@
+﻿using EscuelaMusica.Entidades;
+
+namespace EscuelaMusica.Interfaces
+{
+    public interface IEscuela
+    {
+        Task InsertEscuela(Escuela escuela);
+        Task UpdateEscuela (Escuela escuela);
+        Task<IEnumerable<Escuela>> GetEscuela();
+        Task DeleteEscuela ( int  id); 
+        Task<Escuela> GetEscuelaById (int id);
+    }
+
+}
---
+++ Interfaces/IProfesor.cs
@@ -0,0 +1,13 @@
+﻿using EscuelaMusica.Entidades;
+
+namespace EscuelaMusica.Interfaces
+{
+    public interface IProfesor
+    {
+        Task InsertProfesor(Profesor profesor);
+        Task UpdateProfesor(Profesor profesor);
+        Task<IEnumerable<Profesor>> GetProfesor();
+        Task DeleteProfesor(int id);
+        Task<Profesor> GetProfesorById(int id);
+    }
+}
