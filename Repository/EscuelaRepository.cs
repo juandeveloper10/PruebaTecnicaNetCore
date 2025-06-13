aa355c5aa1d818d426efacc2406e02ca0a8e480e
juandeveloper10 <77118167+juandeveloper10@users.noreply.github.com>
2025-06-13 16:46:04 -0600

    Add files via upload

---
+++ Repository/EscuelaRepository.cs
@@ -0,0 +1,106 @@
+﻿using EscuelaMusica.Entidades;
+using EscuelaMusica.Interfaces;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace EscuelaMusica.Repository
+{
+    public class EscuelaRepository(IConfiguration config) : IEscuela
+    {
+        private string connectionString = config.GetConnectionString("Default");
+        public async Task DeleteEscuela(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+            using var command = new SqlCommand("sp_delete_Escuela", connection)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+
+            };
+            command.Parameters.AddWithValue("@Escuela_Id", id);
+            await connection.OpenAsync();   
+            await command.ExecuteNonQueryAsync();    
+         }
+        public async Task<IEnumerable<Escuela>> GetEscuela()
+        {
+            var escuela = new List<Escuela>();
+            using var connection = new SqlConnection(connectionString);
+            using var command = new SqlCommand("sp_SelectAll_Escuela", connection)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+
+            };
+            await connection.OpenAsync();
+            using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                escuela.Add(new Escuela
+                {
+                    Escuela_Id = reader.GetInt32(0),
+                    Nombre = reader.GetString(1),
+                    Descripcion = reader.GetString(2)   
+                });
+
+
+
+
+            }
+            return escuela;
+
+        }
+
+        public async Task<Escuela> GetEscuelaById(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+            using var command = new SqlCommand("sp_SelectId_Escuela", connection)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+
+            };
+            command.Parameters.AddWithValue("@Escuela_Id", id);
+            await connection.OpenAsync();
+            using var reader  = await command.ExecuteReaderAsync();
+            Escuela _escuela = new();
+            while(await  reader.ReadAsync())
+            {
+                _escuela = new ()
+                {
+                    Escuela_Id = reader.GetInt32(0),
+                    Nombre = reader.GetString(1),
+                    Descripcion = reader.GetString(2)
+
+                };
+            }
+            return _escuela; 
+
+        }
+
+        public async Task InsertEscuela(Escuela escuela)
+        {
+            using var connection = new SqlConnection(connectionString);
+            using var command = new SqlCommand("sp_Ins_Profesor", connection)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+
+            };
+            command.Parameters.AddWithValue("@Nombre", escuela.Nombre);
+            command.Parameters.AddWithValue("@Descripcion", escuela.Descripcion);
+            await connection.OpenAsync();
+            await command.ExecuteReaderAsync(); 
+        }
+
+        public async Task UpdateEscuela(Escuela escuela)
+        {
+            using var connection = new SqlConnection(connectionString);
+            using var command = new SqlCommand("sp_Update_Escuela", connection)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+
+            };
+            command.Parameters.AddWithValue("@Escuela_Id", escuela.Escuela_Id);
+            command.Parameters.AddWithValue("@Nombre", escuela.Nombre);
+            command.Parameters.AddWithValue("@Descripcion", escuela.Descripcion);
+            await connection.OpenAsync();
+            await command.ExecuteReaderAsync();
+        }
+    }
+}
---
+++ Repository/ProfesorRepository.cs
@@ -0,0 +1,123 @@
+﻿using EscuelaMusica.Entidades;
+using EscuelaMusica.Interfaces;
+using Microsoft.Data.SqlClient;
+
+namespace EscuelaMusica.Repository
+{
+    public class ProfesorRepository(IConfiguration config) : IProfesor
+    {
+        private string connectionString = config.GetConnectionString("Default");
+        public async Task DeleteProfesor(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+            using var command = new SqlCommand("sp_delete_Profesor", connection)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+
+            };
+            command.Parameters.AddWithValue("@Profesor_Id", id);
+            await connection.OpenAsync();
+            await command.ExecuteNonQueryAsync();
+        }
+
+       
+
+        public async Task<IEnumerable<Profesor>> GetProfesor()
+        {
+            var profesor = new List<Profesor>();
+            using var connection = new SqlConnection(connectionString);
+            using var command = new SqlCommand("sp_SelectAll_Profesor", connection)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+
+            };
+            await connection.OpenAsync();
+            using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                profesor.Add(new Profesor
+                {
+                    Profesor_Id = reader.GetInt32(0),
+                    Nombre = reader.GetString(1),
+                    Apellido = reader.GetString(2),
+                    FechaNacimiento = reader.GetDateTime(3),
+                    Profesor_Escuela = reader.GetInt32(4),
+
+                });
+
+
+
+
+            }
+            return profesor;
+
+        }
+
+        public async Task<Profesor> GetProfesorById(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+            using var command = new SqlCommand("sp_SelectId_Profesor", connection)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+
+            };
+            command.Parameters.AddWithValue("@Profesor_Id", id);
+            await connection.OpenAsync();
+            using var reader = await command.ExecuteReaderAsync();
+            Profesor _profesor= new();
+            while (await reader.ReadAsync())
+            {
+                _profesor = new()
+                {
+                    Profesor_Id = reader.GetInt32(0),
+                    Nombre = reader.GetString(1),
+                    Apellido = reader.GetString(2),
+                    FechaNacimiento = reader.GetDateTime(3),
+                    Profesor_Escuela = reader.GetInt32(4),
+
+                };
+            }
+            return _profesor;
+
+        }
+
+     
+
+       
+        public async Task InsertProfesor(Profesor profesor)
+        {
+            using var connection = new SqlConnection(connectionString);
+            using var command = new SqlCommand("sp_Ins_Profesor", connection)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+
+            };
+            command.Parameters.AddWithValue("@Nombre", profesor.Nombre);
+            command.Parameters.AddWithValue("@Apellido", profesor.Apellido);
+            command.Parameters.AddWithValue("@FechaNacimiento", profesor.FechaNacimiento);
+            command.Parameters.AddWithValue("@Profesor_Escuela", profesor.Profesor_Escuela);
+            await connection.OpenAsync();
+            await command.ExecuteReaderAsync();
+        }
+
+       
+        public async Task UpdateProfesor(Profesor profesor)
+        {
+            using var connection = new SqlConnection(connectionString);
+            using var command = new SqlCommand("sp_Update_Profesor", connection)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+
+            };
+            command.Parameters.AddWithValue("@Escuela_Id", profesor.Profesor_Id);
+            command.Parameters.AddWithValue("@Nombre", profesor.Nombre);
+            command.Parameters.AddWithValue("@Apellido", profesor.Apellido);
+            command.Parameters.AddWithValue("@FechaNacimiento", profesor.FechaNacimiento);
+            command.Parameters.AddWithValue("@Profesor_Escuela", profesor.Profesor_Escuela);
+            await connection.OpenAsync();
+            await command.ExecuteReaderAsync();
+        }
+
+       
+    }
+}
